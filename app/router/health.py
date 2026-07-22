e35a1b97e11306a8869a0a5655bda7f039825af6
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2026-07-22 23:02:06 +0100

    fix: correct import path

--- app/router/health.py
+++ app/router/health.py
@@ -2,7 +2,7 @@ from fastapi import APIRouter
 from pydantic import BaseModel
 from sqlalchemy import text
 
-from app.core.cache import ping as redis_ping
+from app.db.redis import ping as redis_ping
 from app.deps import DB
 
 
