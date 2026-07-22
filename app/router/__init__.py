c6aef1deb88e606e78ecaaa244221d24c9c097c1
Muizzyranking <oyebowaleabdulmuiz@gmail.com>
2026-07-22 23:02:36 +0100

    feat: routes

--- app/router/__init__.py
+++ app/router/__init__.py
@@ -1,7 +1,11 @@
 from fastapi import APIRouter
 
 from .health import router as health
+from .links import router as links
+from .redirect import router as redirect
 
 router = APIRouter()
 
 router.include_router(health)
+router.include_router(links)
+router.include_router(redirect)
